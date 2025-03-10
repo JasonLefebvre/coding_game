9289b3be6db84308dddf87da9397bce5e238a941
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-03-10 22:09:06 +0100

    equity workshop

---
+++ backend/blog/EquityWorkshopGenerator.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+require("../utils/ConnectToBDD.php");
+
+$new_name = $_POST["name"];
+$new_description = $_POST["description"];
+$new_date = $_POST["date"];
+$new_start_time = $_POST["start_time"];
+$new_final_time = $_POST["finish_time"];
+$type = $_POST["type"];
+
+$sql = "INSERT INTO atelier_equite (nom, description, date, heure_debut, heure_fin, type) VALUES (?, ?, ?, ?, ?, ?)";
+$stmt = $pdo>prepare($sql);
+$stmt->execute([$new_name, $new_description, $new_date, $new_start_time, $new_final_time, $type]);
+
+header("Location: ../../frontend/admin.php");
--- backend/blog/WritingWorkshopGenerator.php
+++ backend/blog/WritingWorkshopGenerator.php
@@ -5,12 +5,12 @@ require("../utils/ConnectToBDD.php");
 
 $new_title = $_POST['titre'];
 $new_description = $_POST['description'];
-$new_day =  $_POST['day'];
+$new_date =  $_POST['date'];
 $new_start_time = $_POST['start_time'];
 $new_final_time = $_POST['finish_time'];
 
-$sql = "INSERT INTO atelier_ecriture (titre, description, date, heure_debut, heure_fin)";
+$sql = "INSERT INTO atelier_ecriture (titre, description, date, heure_debut, heure_fin) VALUES (?, ?, ?, ?, ?)";
 $stmt = $pdo->prepare($sql);
-$stmt->execute([$new_title, $new_description, $new_day, $new_start_time, $new_final_time]);
+$stmt->execute([$new_title, $new_description, $new_date, $new_start_time, $new_final_time]);
 
 header("Location: ../../frontend/admin.php");
