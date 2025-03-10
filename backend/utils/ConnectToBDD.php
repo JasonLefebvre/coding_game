87d8f2386411b9c4aa011506bd4f7df78cfa4c9d
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-03-10 13:13:14 +0100

    .env created

--- backend/utils/ConnectToBDD.php
+++ backend/utils/ConnectToBDD.php
@@ -1,11 +1,17 @@
 <?php
 
+/**
+ * Connexion à la base de données via PDO.
+ *
+ * @global PDO $pdo L'objet PDO pour la connexion à la base de données.
+ * @throws PDOException Si la connexion à la base de données échoue.
+ */
 try {
     global $pdo;
     $pdo = new PDO(
-        'mysql:host=100.83.27.15;dbname=coding_game',
-        'b2-info',
-        '1234',
+        $_ENV['DATABASE_URL'],
+        $_ENV['DATABASE_USER'],
+        $_ENV['DATABASE_PASSWORD'],
         [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
