1caa1f9b4847103626554eff1614334ba2b6ef94
JasonLefebvre <jasonlefebvre25septembre@gmail.com>
2025-03-10 12:55:09 +0100

    bdd

---
+++ utils/ConnectToBDD.php
@@ -0,0 +1,16 @@
+<?php
+
+try {
+    global $pdo;
+    $pdo = new PDO(
+        'mysql:host=100.83.27.15;dbname=coding_game',
+        'b2-info',
+        '1234',
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " .  $e->getMessage());
+}
